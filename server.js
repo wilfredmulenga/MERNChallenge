00d17d45697a151b163a1b818c0b87a8e821333c
WillzMu <wilfredmulenga@yahoo.com>
2019-02-05 22:57:07 +0200

    added primes10000.txt file to project. read the file in server.js and converted it to suitable JSON object for databse

--- server.js
+++ server.js
@@ -4,5 +4,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const fs = require('fs');
+fs.readFile('./primes10000.txt', 'utf8', function(err,data) {
+    if(err) throw err;
+    let obj = {};
+    let splitted = data.toString().split("\n");
+    for (let i = 0; i<splitted.length-2; i++) {
+       let splitline = splitted[i].split(':')
+        obj[splitline[0]] = splitline[1].split(' ',11).slice(1);
+    }
+    console.log(obj);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
