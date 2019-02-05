02f47480d8854eea61b55e9b4c1e2bedd8a7d4a0
WillzMu <wilfredmulenga@yahoo.com>
2019-02-05 21:54:35 +0200

    added validation error when input is more than three digits. plan to add another one when input has letters or special characters

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     super(props)
     this.textInput = React.createRef();
     this.state={
-      value:''
+      value:'',
+      error : ''
     }
     this.handleInputChange = this.handleInputChange.bind(this)
   }
@@ -19,12 +20,17 @@ class App extends Component {
    let input= e.target.value
     if(input.length>3){
      this.textInput.current.style.backgroundColor = 'red'
+     this.setState({
+       error: 'max number is of digits is 3'
+     })
     }else{
       this.textInput.current.style.backgroundColor = null 
+      this.setState({
+        value : input,
+        error : ''
+      })
     }
-    this.setState({
-      value : input
-    })
+    
   }
  
   render() {
@@ -37,9 +43,10 @@ class App extends Component {
             <div>
           <p>{this.state.value}</p>
           </div>
-        <input id='numbers' style={{width:100}} type='number' onChange={this.handleInputChange} value={this.state.value} required
-         pattern="[0-9]" min='000' max='999' maxLength='3' ref={this.textInput}
+        <input id='numbers' style={{width:100}}  onChange={this.handleInputChange} value={this.state.value} required
+         pattern="^[0-9]+$" min='000' max='999'  ref={this.textInput}
         ></input>
+        <p>{this.state.error}</p>
        <div style={{marginTop:30}}>
        <button>Search</button>
        </div>
